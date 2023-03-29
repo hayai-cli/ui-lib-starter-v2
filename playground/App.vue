28d090d389fa19db823f73103e2162c71479abfb
sroxck <642261686@qq.com>
2023-03-29 14:44:52 +0800

    feat: init lib-vue2-starter

---
+++ playground/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Button from '@com/Button/index.vue'
+</script>
+
+<template>
+  <div id="">
+  <Button></Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
